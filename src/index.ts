347154ccf9bce8cc5492ff4f6a01a41e8416d88a
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2026-09-01 00:09:41 -0300

    add vulnerability

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 
+const AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE";
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
